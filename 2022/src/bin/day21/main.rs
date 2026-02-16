2bdc72dca799da1a7d075d74cb313e42c91458d2
Luca Lencioni <l.lencioni@hastega.it>
2026-02-16 12:09:29 +0100

    shitty d21 and p1 d22

--- 2022/src/bin/day21/main.rs
+++ 2022/src/bin/day21/main.rs
@@ -1,25 +1,126 @@
+use regex::Regex;
+use std::collections::{HashMap, HashSet};
+
 /// Advent of Code 2022 - Day 21
 /// https://adventofcode.com/2022/day/21
 fn main() {
     let input = include_str!("./input.txt");
-    part1(&input);
-    // part2(&input);
+    // part1(&input);
+    part2(&input);
 }
 
 #[allow(unused)]
 fn part1(input: &str) {
     let lines = input.lines();
 
+    let re_num = Regex::new(r"^(\w+):\s(\d+)$").unwrap();
+    let re_op = Regex::new(r"^(\w+):\s(\w+)\s*(.)\s*(\w+)$").unwrap();
+    let mut nums = HashMap::<&str, i64>::new();
+    let mut ops: HashSet<(&str, &str, &str, &str)> = HashSet::new();
     for line in lines {
-        let mut chars = line.chars();
+        if re_num.is_match(line) {
+            let (_, [monkey, num_str]) = re_num.captures(line).unwrap().extract();
+            let num = num_str.parse::<i64>().unwrap();
+            nums.insert(monkey, num);
+            continue;
+        }
+        let (_, [monkey, var_1, op, var_2]) = re_op.captures(line).unwrap().extract();
+        if nums.contains_key(var_1) && nums.contains_key(var_2) {
+            // it can be resolved immediatly
+            let n1 = nums.get(var_1).unwrap();
+            let n2 = nums.get(var_2).unwrap();
+            let res = process_op(*n1, op, *n2);
+            nums.insert(monkey, res);
+        }
+        ops.insert((monkey, var_1, op, var_2));
+    }
+
+    while !nums.contains_key("root") {
+        for operation in ops.clone().iter() {
+            let &(monkey, var_1, op, var_2) = operation;
+            if nums.contains_key(var_1) && nums.contains_key(var_2) {
+                // it can be resolved immediatly
+                let n1 = nums.get(var_1).unwrap();
+                let n2 = nums.get(var_2).unwrap();
+                let res = process_op(*n1, op, *n2);
+                nums.insert(monkey, res);
+                ops.remove(operation);
+            }
+        }
+    }
+
+    print!("{}", nums.get("root").unwrap())
+}
+
+fn process_op(v1: i64, op: &str, v2: i64) -> i64 {
+    match op {
+        "-" => v1 - v2,
+        "+" => v1 + v2,
+        "*" => v1 * v2,
+        "/" => v1 / v2,
+        _ => unreachable!(),
     }
 }
 
 #[allow(unused)]
 fn part2(input: &str) {
     let lines = input.lines();
-
+    let re_num = Regex::new(r"^(\w+):\s(\d+)$").unwrap();
+    let re_op = Regex::new(r"^(\w+):\s(\w+)\s*(.)\s*(\w+)$").unwrap();
+    let mut nums = HashMap::<&str, i64>::new();
+    let mut ops: HashSet<(&str, &str, &str, &str)> = HashSet::new();
     for line in lines {
-        let mut chars = line.chars();
+        if re_num.is_match(line) {
+            let (_, [monkey, num_str]) = re_num.captures(line).unwrap().extract();
+            let num = num_str.parse::<i64>().unwrap();
+            if monkey == "humn" {
+                continue;
+            }
+            nums.insert(monkey, num);
+            continue;
+        }
+        let (_, [monkey, var_1, op, var_2]) = re_op.captures(line).unwrap().extract();
+        if nums.contains_key(var_1) && nums.contains_key(var_2) {
+            // it can be resolved immediatly
+            let n1 = nums.get(var_1).unwrap();
+            let n2 = nums.get(var_2).unwrap();
+            let res = process_op(*n1, op, *n2);
+            nums.insert(monkey, res);
+        }
+        ops.insert((monkey, var_1, op, var_2));
     }
+    let mut humn: i64 = 0; // 3 887 609 741 189
+    let mut inc: i64 = 1;
+    loop {
+        let mut nums_clone = nums.clone();
+        nums_clone.insert("humn", humn);
+        let mut ops_clone = ops.clone();
+        while !nums_clone.contains_key("root") {
+            for operation in ops_clone.clone().iter() {
+                let &(monkey, var_1, op, var_2) = operation;
+                if nums_clone.contains_key(var_1) && nums_clone.contains_key(var_2) {
+                    // it can be resolved immediatly
+                    let n1 = nums_clone.get(var_1).unwrap();
+                    let n2 = nums_clone.get(var_2).unwrap();
+                    if monkey == "root" {
+                        if n1 == n2 {
+                            println!("{humn}");
+                            return;
+                        }
+                        inc = (n1.abs_diff(*n2) as i64) / 2;
+                        if inc == 0 {
+                            inc = 1;
+                        }
+                        println!("humn: {humn}, inc: {inc}, {n1} {n2} {}", n1 > n2)
+                    }
+                    let res = process_op(*n1, op, *n2);
+                    nums_clone.insert(monkey, res);
+                    ops_clone.remove(operation);
+                }
+            }
+        }
+        humn = humn + inc;
+    }
+
+    print!("{}", nums.get("root").unwrap())
 }
--- 2022/src/bin/day22/main.rs
+++ 2022/src/bin/day22/main.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+
+use regex::Regex;
+
 /// Advent of Code 2022 - Day 22
 /// https://adventofcode.com/2022/day/22
 fn main() {
@@ -9,10 +13,131 @@ fn main() {
 #[allow(unused)]
 fn part1(input: &str) {
     let lines = input.lines();
-
-    for line in lines {
+    let last_line = lines.clone().last().unwrap();
+    let mut map = HashMap::<(i32, i32), char>::new();
+    let mut pos: (i32, i32) = (0, 0);
+    // 0 -> Right
+    // 1 -> Down
+    // 2 -> Left
+    // 3 -> Up
+    let mut dir = 0;
+    for (row, line) in lines.enumerate() {
+        if line.trim().is_empty() {
+            break;
+        }
         let mut chars = line.chars();
+        for (col, ch) in chars.enumerate() {
+            match ch {
+                ' ' | '\n' | '\t' | '\r' => continue,
+                _ => {
+                    if map.len() == 0 {
+                        pos = (row as i32 + 1, col as i32 + 1)
+                    }
+                    map.insert((row as i32 + 1, col as i32 + 1), ch);
+                }
+            }
+        }
     }
+    let re = Regex::new(r"(\d+)|([A-Z]+)").unwrap();
+    for capture in re.captures_iter(last_line) {
+        match capture.get_match().as_str() {
+            "R" => dir = (dir + 1) % 4,
+            "L" => {
+                if dir == 0 {
+                    dir = 3
+                } else {
+                    dir = (dir - 1)
+                }
+            }
+            n => {
+                let num = n.parse::<u32>().unwrap();
+                'outer: for _ in 0..num {
+                    let next_pos = match dir {
+                        0 => (pos.0, pos.1 + 1),
+                        1 => (pos.0 + 1, pos.1),
+                        2 => (pos.0, pos.1 - 1),
+                        3 => (pos.0 - 1, pos.1),
+                        _ => unreachable!(),
+                    };
+                    match map.get(&next_pos) {
+                        Some(ch) => match ch {
+                            '.' => {
+                                pos = next_pos;
+                                println!("{},{}", pos.0, pos.1);
+                            }
+                            '#' => break 'outer,
+                            _ => unreachable!(),
+                        },
+                        None => {
+                            // wrap around
+                            match dir {
+                                0 => {
+                                    let positions = map
+                                        .keys()
+                                        .filter(|&p| p.0 == pos.0)
+                                        .collect::<Vec<&(i32, i32)>>();
+                                    let wrap_coord = positions.iter().fold(pos.1, |init, &p| {
+                                        if p.1 < init { p.1 } else { init }
+                                    });
+                                    let wrap_coords = (pos.0, wrap_coord);
+                                    match map.get(&wrap_coords) {
+                                        Some('.') => pos = wrap_coords,
+                                        _ => break,
+                                    }
+                                }
+                                1 => {
+                                    let positions = map
+                                        .keys()
+                                        .filter(|&p| p.1 == pos.1)
+                                        .collect::<Vec<&(i32, i32)>>();
+                                    let wrap_coord = positions.iter().fold(pos.0, |init, &p| {
+                                        if p.0 < init { p.0 } else { init }
+                                    });
+                                    let wrap_coords = (wrap_coord, pos.1);
+                                    match map.get(&wrap_coords) {
+                                        Some('.') => pos = wrap_coords,
+                                        _ => break,
+                                    }
+                                }
+                                2 => {
+                                    let positions = map
+                                        .keys()
+                                        .filter(|&p| p.0 == pos.0)
+                                        .collect::<Vec<&(i32, i32)>>();
+                                    let wrap_coord = positions.iter().fold(pos.1, |init, &p| {
+                                        if p.1 > init { p.1 } else { init }
+                                    });
+                                    let wrap_coords = (pos.0, wrap_coord);
+                                    match map.get(&wrap_coords) {
+                                        Some('.') => pos = wrap_coords,
+                                        _ => break,
+                                    }
+                                }
+                                3 => {
+                                    let positions = map
+                                        .keys()
+                                        .filter(|&p| p.1 == pos.1)
+                                        .collect::<Vec<&(i32, i32)>>();
+                                    let wrap_coord = positions.iter().fold(pos.0, |init, &p| {
+                                        if p.0 > init { p.0 } else { init }
+                                    });
+                                    let wrap_coords = (wrap_coord, pos.1);
+                                    match map.get(&wrap_coords) {
+                                        Some('.') => pos = wrap_coords,
+                                        _ => break,
+                                    }
+                                }
+                                _ => unreachable!(),
+                            }
+                            println!("{},{}", pos.0, pos.1);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    println!("{},{} {}", pos.0, pos.1, dir);
+    println!("{}", 1000 * pos.0 + 4 * pos.1 + dir)
 }
 
 #[allow(unused)]
