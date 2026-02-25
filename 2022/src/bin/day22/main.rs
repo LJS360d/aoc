1e7949f4cb890527da72f6bfb6b11a51c23b3d98
Luca Lencioni <l.lencioni@hastega.it>
2026-02-25 10:30:21 +0100

    d22 (bad) d23 p1

--- 2022/src/bin/day22/main.rs
+++ 2022/src/bin/day22/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use regex::Regex;
 
@@ -6,8 +6,8 @@ use regex::Regex;
 /// https://adventofcode.com/2022/day/22
 fn main() {
     let input = include_str!("./input.txt");
-    part1(&input);
-    // part2(&input);
+    // part1(&input);
+    part2(&input);
 }
 
 #[allow(unused)]
@@ -140,11 +140,187 @@ fn part1(input: &str) {
     println!("{}", 1000 * pos.0 + 4 * pos.1 + dir)
 }
 
+type Coords = (i32, i32);
+
+#[derive(Eq, Hash, PartialEq)]
+struct Node {
+    pos: Coords,
+    val: char,
+}
+
+struct Edge {
+    src: Coords,
+    trg: Coords,
+    dir: i32,
+}
+
+struct Graph {
+    nodes: HashSet<Node>,
+    edges: HashSet<Edge>,
+}
+
+impl Graph {
+    fn new() -> Self {
+        Self {
+            nodes: HashSet::new(),
+            edges: HashSet::new(),
+        }
+    }
+}
+
 #[allow(unused)]
 fn part2(input: &str) {
     let lines = input.lines();
-
-    for line in lines {
+    let last_line = lines.clone().last().unwrap();
+    let mut graph = Graph::new();
+    const BOUND: i32 = 50;
+    let mut pos = (0, 0);
+    // Directions:
+    // 0 -> Right
+    // 1 -> Down
+    // 2 -> Left
+    // 3 -> Up
+    let mut dir = 0;
+    for (row, line) in lines.enumerate() {
+        if line.trim().is_empty() {
+            break;
+        }
         let mut chars = line.chars();
+        for (col, ch) in chars.enumerate() {
+            match ch {
+                ' ' | '\n' | '\t' | '\r' => continue,
+                '.' => {
+                    if graph.nodes.len() == 0 {
+                        pos = (row as i32 + 1, col as i32 + 1)
+                    }
+                    let node_pos = (row as i32 + 1, col as i32 + 1);
+                    graph.nodes.insert(Node {
+                        pos: node_pos,
+                        val: ch,
+                    });
+                    let right = (node_pos.0, node_pos.1 + 1);
+                    match chars.nth(col + 1) {
+                        Some(r) => {
+                            match r {
+                                '#' => continue,
+                                '.' => {
+                                    graph.edges.insert(Edge{
+                                        dir: 0,
+
+                                    })
+                                }
+                            }
+                        },
+                        None => {
+                            // 2->5
+                            //
+                        }
+                    }
+                    let left = (node_pos.0, node_pos.1 - 1);
+                    let down = (node_pos.0 + 1, node_pos.1);
+                    let up = (node_pos.0 - 1, node_pos.1);
+                }
+            }
+        }
     }
+    let re = Regex::new(r"(\d+)|([A-Z]+)").unwrap();
+    for capture in re.captures_iter(last_line) {
+        match capture.get_match().as_str() {
+            "R" => dir = (dir + 1) % 4,
+            "L" => {
+                if dir == 0 {
+                    dir = 3
+                } else {
+                    dir = (dir - 1)
+                }
+            }
+            n => {
+                let num = n.parse::<u32>().unwrap();
+                'outer: for _ in 0..num {
+                    let next_pos = match dir {
+                        0 => (pos.0, pos.1 + 1),
+                        1 => (pos.0 + 1, pos.1),
+                        2 => (pos.0, pos.1 - 1),
+                        3 => (pos.0 - 1, pos.1),
+                        _ => unreachable!(),
+                    };
+                    match map.get(&next_pos) {
+                        Some(ch) => match ch {
+                            '.' => {
+                                pos = next_pos;
+                                println!("{},{}", pos.0, pos.1);
+                            }
+                            '#' => break 'outer,
+                            _ => unreachable!(),
+                        },
+                        None => {
+                            // wrap around
+                            match dir {
+                                0 => {
+                                    let positions = map
+                                        .keys()
+                                        .filter(|&p| p.0 == pos.0)
+                                        .collect::<Vec<&(i32, i32)>>();
+                                    let wrap_coord = positions.iter().fold(pos.1, |init, &p| {
+                                        if p.1 < init { p.1 } else { init }
+                                    });
+                                    let wrap_coords = (pos.0, wrap_coord);
+                                    match map.get(&wrap_coords) {
+                                        Some('.') => pos = wrap_coords,
+                                        _ => break,
+                                    }
+                                }
+                                1 => {
+                                    let positions = map
+                                        .keys()
+                                        .filter(|&p| p.1 == pos.1)
+                                        .collect::<Vec<&(i32, i32)>>();
+                                    let wrap_coord = positions.iter().fold(pos.0, |init, &p| {
+                                        if p.0 < init { p.0 } else { init }
+                                    });
+                                    let wrap_coords = (wrap_coord, pos.1);
+                                    match map.get(&wrap_coords) {
+                                        Some('.') => pos = wrap_coords,
+                                        _ => break,
+                                    }
+                                }
+                                2 => {
+                                    let positions = map
+                                        .keys()
+                                        .filter(|&p| p.0 == pos.0)
+                                        .collect::<Vec<&(i32, i32)>>();
+                                    let wrap_coord = positions.iter().fold(pos.1, |init, &p| {
+                                        if p.1 > init { p.1 } else { init }
+                                    });
+                                    let wrap_coords = (pos.0, wrap_coord);
+                                    match map.get(&wrap_coords) {
+                                        Some('.') => pos = wrap_coords,
+                                        _ => break,
+                                    }
+                                }
+                                3 => {
+                                    let positions = map
+                                        .keys()
+                                        .filter(|&p| p.1 == pos.1)
+                                        .collect::<Vec<&(i32, i32)>>();
+                                    let wrap_coord = positions.iter().fold(pos.0, |init, &p| {
+                                        if p.0 > init { p.0 } else { init }
+                                    });
+                                    let wrap_coords = (wrap_coord, pos.1);
+                                    match map.get(&wrap_coords) {
+                                        Some('.') => pos = wrap_coords,
+                                        _ => break,
+                                    }
+                                }
+                                _ => unreachable!(),
+                            }
+                            println!("{},{}", pos.0, pos.1);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    println!("{},{} {}", pos.0, pos.1, dir);
+    println!("{}", 1000 * pos.0 + 4 * pos.1 + dir)
 }
--- 2022/src/bin/day23/main.rs
+++ 2022/src/bin/day23/main.rs
@@ -1,18 +1,112 @@
 /// Advent of Code 2022 - Day 23
 /// https://adventofcode.com/2022/day/23
 fn main() {
-    let input = include_str!("./input.txt");
+    let input = include_str!("./test_input.txt");
     part1(&input);
     // part2(&input);
 }
 
+type Coords = (i32, i32);
+
+#[derive(Eq, Hash, PartialEq, Clone, Copy)]
+struct Elf {
+    pos: Coords,
+    dest: Option<Coords>,
+}
+
 #[allow(unused)]
 fn part1(input: &str) {
-    let lines = input.lines();
-
-    for line in lines {
-        let mut chars = line.chars();
+    let mut elves: Vec<Elf> = vec![];
+    for (row, line) in input.lines().enumerate() {
+        for (col, char) in line.chars().enumerate() {
+            elves.push(Elf {
+                pos: (row as i32, col as i32),
+                dest: None,
+            });
+        }
     }
+    const ROUNDS: i32 = 10;
+    for _ in 0..ROUNDS {
+        // phase 1: Proposals
+        let cloned_elves = elves.clone();
+        for elf in elves.iter_mut() {
+            // check N, NE, NW
+            let n = (elf.pos.0 - 1, elf.pos.1);
+            let ne = (elf.pos.0 - 1, elf.pos.1 + 1);
+            let nw = (elf.pos.0 - 1, elf.pos.1 - 1);
+            if !cloned_elves
+                .iter()
+                .any(|e| e.pos == n || e.pos == ne || e.pos == nw)
+            {
+                elf.dest = Some(n);
+                continue;
+            }
+            // check S, SE, SW
+            let s = (elf.pos.0 + 1, elf.pos.1);
+            let se = (elf.pos.0 + 1, elf.pos.1 + 1);
+            let sw = (elf.pos.0 + 1, elf.pos.1 - 1);
+            if !cloned_elves
+                .iter()
+                .any(|e| e.pos == s || e.pos == se || e.pos == sw)
+            {
+                elf.dest = Some(s);
+                continue;
+            }
+            // check W, NW, SW
+            let w = (elf.pos.0, elf.pos.1 - 1);
+            if !cloned_elves
+                .iter()
+                .any(|e| e.pos == w || e.pos == nw || e.pos == sw)
+            {
+                elf.dest = Some(w);
+                continue;
+            }
+            // check E, NE, SE
+            let e = (elf.pos.0, elf.pos.1 + 1);
+            if !cloned_elves
+                .iter()
+                .any(|el| el.pos == e || el.pos == ne || el.pos == se)
+            {
+                elf.dest = Some(e);
+                continue;
+            }
+        }
+        let elves_with_dest = elves
+            .clone()
+            .into_iter()
+            .filter(|&elf| elf.dest.is_some())
+            .collect::<Vec<Elf>>();
+        // phase 2: Movement
+        for elf in elves.iter_mut() {
+            match elf.dest {
+                None => continue,
+                Some(dest) => {
+                    if !elves_with_dest.iter().any(|&ce| ce.dest.unwrap() == dest) {
+                        elf.pos = dest;
+                        println!("Elf moving to {},{}", dest.0, dest.1)
+                    }
+                }
+            }
+        }
+    }
+
+    println!("{}", count_ground(&elves))
+}
+
+fn count_ground(elves: &Vec<Elf>) -> i32 {
+    let (min_row, max_row) = elves.iter().fold((i32::MAX, i32::MIN), |(min, max), elf| {
+        let new_min = std::cmp::min(min, elf.pos.0);
+        let new_max = std::cmp::max(max, elf.pos.0);
+        (new_min, new_max)
+    });
+    let (min_col, max_col) = elves.iter().fold((i32::MAX, i32::MIN), |(min, max), elf| {
+        let new_min = std::cmp::min(min, elf.pos.1);
+        let new_max = std::cmp::max(max, elf.pos.1);
+        (new_min, new_max)
+    });
+    let width = max_col - min_col;
+    let height = max_row - min_row;
+    (width * height) - elves.len() as i32
 }
 
 #[allow(unused)]
