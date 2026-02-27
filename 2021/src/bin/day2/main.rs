25b6d9fc73ad9d36497a06925f0481a7dde49eae
Luca Lencioni <l.lencioni@hastega.it>
2026-02-27 14:39:10 +0100

    2021 d2

--- 2021/src/bin/day2/main.rs
+++ 2021/src/bin/day2/main.rs
@@ -2,24 +2,56 @@
 /// https://adventofcode.com/2021/day/2
 fn main() {
     let input = include_str!("./input.txt");
-    part1(&input);
-    // part2(&input);
+    // part1(&input);
+    part2(&input);
 }
 
 #[allow(unused)]
 fn part1(input: &str) {
-    let lines = input.lines();
-
-    for line in lines {
-        let mut chars = line.chars();
+    let mut depth = 0;
+    let mut h = 0;
+    for line in input.lines() {
+        let arr: Vec<&str> = line.split(" ").collect();
+        let (dir, amount_str) = (arr[0], arr[1]);
+        let amount: i32 = amount_str.parse().unwrap();
+        match dir {
+            "forward" => {
+                h = h + amount;
+            }
+            "up" => {
+                depth = depth - amount;
+            }
+            "down" => {
+                depth = depth + amount;
+            }
+            _ => unreachable!(),
+        }
     }
+    println!("{}", h * depth);
 }
 
 #[allow(unused)]
 fn part2(input: &str) {
-    let lines = input.lines();
-
-    for line in lines {
-        let mut chars = line.chars();
+    let mut depth: i64 = 0;
+    let mut aim: i64 = 0;
+    let mut h: i64 = 0;
+    for line in input.lines() {
+        let arr: Vec<&str> = line.split(" ").collect();
+        let (dir, amount_str) = (arr[0], arr[1]);
+        let amount: i64 = amount_str.parse().unwrap();
+        match dir {
+            "forward" => {
+                h = h + amount;
+                depth = depth + aim * amount;
+            }
+            "up" => {
+                aim = aim - amount;
+            }
+            "down" => {
+                aim = aim + amount;
+            }
+            _ => unreachable!(),
+        }
     }
+    println!("{}", h * depth);
 }
