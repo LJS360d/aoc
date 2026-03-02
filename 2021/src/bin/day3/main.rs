92d15c0d20d317f13bd9bf4d17b81fdf0dc9faa7
Luca Lencioni <l.lencioni@hastega.it>
2026-03-02 15:30:56 +0100

    2021 d3-1 d4-1/2

--- 2021/src/bin/day3/main.rs
+++ 2021/src/bin/day3/main.rs
@@ -1,25 +1,66 @@
+use std::collections::{HashMap, HashSet};
+
 /// Advent of Code 2021 - Day 3
 /// https://adventofcode.com/2021/day/3
 fn main() {
     let input = include_str!("./input.txt");
-    part1(&input);
-    // part2(&input);
+    // part1(&input);
+    part2(&input);
 }
 
 #[allow(unused)]
 fn part1(input: &str) {
-    let lines = input.lines();
+    let bins: Vec<&str> = input.lines().collect();
+    let width = bins[0].len();
+    let total = bins.len();
 
-    for line in lines {
-        let mut chars = line.chars();
-    }
+    let gamma_str: String = (0..width)
+        .map(|i| {
+            let ones = bins.iter().filter(|s| s.as_bytes()[i] == b'1').count();
+            if ones > total / 2 { '1' } else { '0' }
+        })
+        .collect();
+
+    let gamma = i32::from_str_radix(&gamma_str, 2).unwrap();
+    let epsilon = !gamma & ((1 << width) - 1);
+
+    println!("{}", gamma * epsilon);
 }
 
 #[allow(unused)]
 fn part2(input: &str) {
-    let lines = input.lines();
-
-    for line in lines {
-        let mut chars = line.chars();
+    let bins: Vec<&str> = input.lines().map(|s| s).collect();
+    let bins_set: HashSet<&str> = bins.iter().cloned().collect();
+    let width = bins[0].len();
+    let mut counts: HashMap<usize, i32> = HashMap::new();
+    for i in 0..width {
+        for bin in bins.clone() {
+            let ch = bin.chars().nth(i).unwrap();
+            if ch == '1' {
+                let entry = counts.entry(i).or_insert(0);
+                *entry += 1;
+            }
+        }
     }
+
+    let mut oxygen_bins_set = bins_set.clone();
+    println!("{:?}", oxygen_bins_set);
+    for bin in bins_set {
+        if oxygen_bins_set.len() == 1 {
+            break;
+        }
+        for i in 0..width {
+            let count = counts.get(&i).unwrap();
+            let ch = if *count >= (bins.len() as i32 / 2) {
+                '1'
+            } else {
+                '0'
+            };
+            if bin.chars().nth(i).unwrap() != ch {
+                oxygen_bins_set.remove(bin);
+                break;
+            }
+        }
+    }
+    println!("{:?}", oxygen_bins_set);
 }
--- 2021/src/bin/day4/main.rs
+++ 2021/src/bin/day4/main.rs
@@ -1,25 +1,188 @@
+use regex::Match;
+
 /// Advent of Code 2021 - Day 4
 /// https://adventofcode.com/2021/day/4
 fn main() {
     let input = include_str!("./input.txt");
-    part1(&input);
-    // part2(&input);
+    // part1(&input);
+    part2(&input);
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, Hash)]
+struct Cell {
+    value: usize,
+    marked: bool,
+}
+
+impl std::fmt::Debug for Cell {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self.value)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct Board {
+    grid: Vec<Vec<Cell>>,
+    winner: bool,
+}
+
+impl Board {
+    fn row(&self, y: usize) -> Vec<Cell> {
+        self.grid[y].clone()
+    }
+
+    fn col(&self, x: usize) -> Vec<Cell> {
+        self.grid
+            .iter()
+            .map(|row| row[x])
+            .collect::<Vec<Cell>>()
+            .clone()
+    }
+
+    fn get_unmarked(&self) -> Vec<Cell> {
+        self.grid
+            .iter()
+            .flat_map(|row| row.iter())
+            .filter(|c| !c.marked)
+            .cloned()
+            .collect::<Vec<Cell>>()
+    }
+
+    fn mark(&mut self, n: usize) {
+        for row in self.grid.iter_mut() {
+            for cell in row.iter_mut() {
+                if cell.value == n {
+                    cell.marked = true;
+                }
+            }
+        }
+    }
+
+    fn has_winner(&self) -> Option<Vec<Cell>> {
+        for n in 0..5 {
+            let row = self.row(n);
+            if row.iter().all(|c| c.marked) {
+                return Some(row);
+            }
+            let col = self.col(n);
+            if col.iter().all(|c| c.marked) {
+                return Some(col);
+            }
+        }
+        None
+    }
 }
 
 #[allow(unused)]
 fn part1(input: &str) {
-    let lines = input.lines();
-
+    let mut lines = input.lines();
+    let seq_str = lines.next().unwrap();
+    let mut board_idx: isize = -1;
+    let mut y: usize = 0;
+    let mut boards: Vec<Board> = vec![];
+    let re = regex::Regex::new(r"(\s?\d+)").unwrap();
     for line in lines {
-        let mut chars = line.chars();
+        if line == "" {
+            y = 0;
+            board_idx += 1;
+            boards.push(Board {
+                grid: vec![
+                    vec![
+                        Cell {
+                            value: 0,
+                            marked: false
+                        };
+                        5
+                    ];
+                    5
+                ],
+                winner: false,
+            });
+            continue;
+        }
+        let mut board = &mut boards[board_idx as usize];
+        let row: Vec<Match> = re.find_iter(line).collect();
+        for (i, s) in row.iter().enumerate() {
+            let mut n = s.as_str().trim().parse::<usize>().unwrap();
+            board.grid[y][i].value = n;
+        }
+        y += 1;
+    }
+
+    let seq = seq_str
+        .split(",")
+        .map(|s| s.parse::<usize>().unwrap())
+        .collect::<Vec<usize>>();
+
+    for num in seq {
+        for board in boards.iter_mut() {
+            board.mark(num);
+            if let Some(_) = board.has_winner() {
+                let sum: usize = board.get_unmarked().iter().map(|c| c.value).sum();
+                println!("{:?}", sum * num);
+                return;
+            } else {
+                continue;
+            }
+        }
     }
 }
 
 #[allow(unused)]
 fn part2(input: &str) {
-    let lines = input.lines();
-
+    let mut lines = input.lines();
+    let seq_str = lines.next().unwrap();
+    let mut board_idx: isize = -1;
+    let mut y: usize = 0;
+    let mut boards: Vec<Board> = vec![];
+    let re = regex::Regex::new(r"(\s?\d+)").unwrap();
     for line in lines {
-        let mut chars = line.chars();
+        if line == "" {
+            y = 0;
+            board_idx += 1;
+            boards.push(Board {
+                grid: vec![
+                    vec![
+                        Cell {
+                            value: 0,
+                            marked: false
+                        };
+                        5
+                    ];
+                    5
+                ],
+                winner: false,
+            });
+            continue;
+        }
+        let mut board = &mut boards[board_idx as usize];
+        let row: Vec<Match> = re.find_iter(line).collect();
+        for (i, s) in row.iter().enumerate() {
+            let mut n = s.as_str().trim().parse::<usize>().unwrap();
+            board.grid[y][i].value = n;
+        }
+        y += 1;
+    }
+
+    let seq = seq_str
+        .split(",")
+        .map(|s| s.parse::<usize>().unwrap())
+        .collect::<Vec<usize>>();
+
+    for num in seq {
+        let mut boards_clone = boards.clone();
+        for board in boards.iter_mut() {
+            if board.winner {
+                continue;
+            }
+            board.mark(num);
+            if let Some(_) = board.has_winner() {
+                board.winner = true;
+                let sum: usize = board.get_unmarked().iter().map(|c| c.value).sum();
+                println!("{:?}", sum * num);
+            } else {
+                continue;
+            }
+        }
     }
 }
